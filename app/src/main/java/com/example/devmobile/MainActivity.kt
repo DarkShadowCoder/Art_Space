b25a02d2f76fadba97bf492ab9f04e2f87cfa1c1
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-10-15 17:03:48 +0100

    Updated data

--- app/src/main/java/com/example/devmobile/MainActivity.kt
+++ app/src/main/java/com/example/devmobile/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -32,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -76,82 +79,109 @@ fun ArtSpaceCard(items: List<Item>,
     Column(
         modifier = modifier
             .fillMaxSize()
-            .verticalScroll(scrollState)
-            .padding(16.dp),
+            .verticalScroll(scrollState),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween) {
-
-        Card(
+        Box (
             modifier = modifier
                 .height(78.dp)
-                .padding(5.dp)
+                .shadow(2.dp)
                 .fillMaxWidth(),
-            border = BorderStroke(1.dp, Color.Gray),
-            shape = RoundedCornerShape(16.dp),
+            contentAlignment = Alignment.Center,
 
         ) {
-            Column(
+            Text(text = "Art Space (${currentIndex+1}/4)",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold)
+        }
+
+        Column(modifier = modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+            ) {
+            Image(
+                painter = painterResource(id = currentItem.imageRes),
+                contentDescription = currentItem.title,
                 modifier = modifier
+                    .width(800.dp)
+                    .height(400.dp)
+                    .padding(5.dp)
+                    .shadow(3.dp, shape = RoundedCornerShape(16.dp)),
+                alignment = Alignment.Center,
+                contentScale = ContentScale.FillHeight
+            )
+            Spacer(modifier = modifier.height(45.dp).fillMaxWidth())
+            Card(
+                modifier = modifier
+                    .height(98.dp)
+                    .padding(5.dp)
                     .fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text(
-                    text = currentItem.title,
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Text(
-                    text = currentItem.description,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Normal
-                )
-            }
-
-        }
-
-        Image(painter = painterResource(id = currentItem.imageRes), contentDescription = currentItem.title, modifier = modifier
-            .width(800.dp)
-            .height(400.dp)
-            .shadow(3.dp, shape = RoundedCornerShape(16.dp)))
-
-        Row(
-            modifier = modifier
-                .height(76.dp)
-                .fillMaxWidth()
-                .padding(14.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween) {
-
-            Button(
-                modifier = modifier
-                    .height(45.dp)
-                    .width(125.dp),
-                onClick = {currentIndex = if(currentIndex > 0) currentIndex -1 else items.size -1}
-            ){
-                Text(
-                    text = "Previous",
-                    fontSize = 13.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color(249, 250, 248, 255)
-                )
-
-            }
-
-            Button(
-                modifier = modifier
-                    .height(45.dp)
-                    .width(125.dp),
-                onClick = {currentIndex = (currentIndex +1) % items.size}
-            ){
-                Text(
-                    text = "Next",
-                    fontSize = 13.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color(249, 252, 248, 255)
-                )
-
+                shape = RoundedCornerShape(5.dp),
+                ) {
+                Column(
+                    modifier = modifier
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = currentItem.title,
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = modifier.padding(vertical = 5.dp)
+                    )
+                    Text(
+                        text = currentItem.description,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+                }
             }
         }
+
+            Row(
+                modifier = modifier
+                    .height(76.dp)
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                Button(
+                    modifier = modifier
+                        .height(45.dp)
+                        .width(145.dp),
+                    onClick = {
+                        currentIndex = if (currentIndex > 0) currentIndex - 1 else items.size - 1
+                    }
+                ) {
+                    Text(
+                        text = "Previous",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(255, 255, 245, 255)
+                    )
+
+                }
+
+                Button(
+                    modifier = modifier
+                        .height(45.dp)
+                        .width(145.dp),
+                    onClick = { currentIndex = (currentIndex + 1) % items.size }
+                ) {
+                    Text(
+                        text = "Next",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(255, 255, 255, 255)
+                    )
+
+                }
+            }
+
 
     }
 
@@ -162,9 +192,10 @@ fun ArtSpaceCard(items: List<Item>,
 fun ArtSpacePreview(){
     // Liste des elements à afficher à chaque changement d'états
     val items = listOf(
-        Item(R.drawable.mona_lisa, "Mona Lisa", "Leonard de Vinci, Oeuvre d'art le plus reconue"),
-        Item(R.drawable.last_supper, "La Cène", "Leonard de Vinci, 1495-1498"),
-        Item(R.drawable.starry_night, "Nuit étoilée", "Van Gogh, 1889, Musée d'art New York")
+        Item(R.drawable.reunification, "Reunification", "Celebre monument situé à Yaoundé"),
+        Item(R.drawable.yaounde_cameroon, "Monument du Cameroun", "Sous le nom de ' J'aime mon pays' "),
+        Item(R.drawable.pagode, "Pagoda", "Monument celebre de Thailand"),
+        Item(R.drawable.chute_melong, "Chute de Melong", "Celebre chute de la ville de Melong Cameroun")
     )
     
     DevMobileTheme {
