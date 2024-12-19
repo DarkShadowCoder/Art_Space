8229327bf61a4af4ea48f245e7dddd53cda4c8d5
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-12-19 08:26:56 +0100

    Delete AppContainer.kt

--- app/src/main/java/data/AppContainer.kt
+++
@@ -1,8 +0,0 @@
-package data
-
-import com.example.devmobile.AppDatabase
-import com.example.devmobile.FlightRepository
-
-override val flightsRepository: FlightRepository by lazy {
-    OfflineFlightRepository (AppDatabase.getDatabase(context).flightDao())
-}
