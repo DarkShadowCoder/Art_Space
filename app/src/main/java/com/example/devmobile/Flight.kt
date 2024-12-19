f96d692d22d76fe779766ff6751c8ab6cab5d4c7
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-12-19 08:26:44 +0100

    Update Flight.kt

--- app/src/main/java/com/example/devmobile/Flight.kt
+++ app/src/main/java/com/example/devmobile/Flight.kt
@@ -6,14 +6,20 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "airport")
 data class Airport(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "iata_code") val iata_code: String,
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "passengers") val passengers: Int,
+    @PrimaryKey(autoGenerate= true)
+    val id: Int = 0,
+    @ColumnInfo(name = "iata_code")
+    val iata_code: String,
+    val name: String,
+    val passengers: Int
 )
-@Entity(tabelName = "favorite")
+
+@Entity(tableName = "favorite")
 data class Favorite(
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "departure_code") val departure_code: String,
-    @ColumnInfo(name = "destination_code") val destination_code: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "departure_code")
+    val departure_code: String,
+    @ColumnInfo(name = "destination_code")
+    val destination_code: String
 )
