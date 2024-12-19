b0059ea726e2f02b58bbad7478a2715a324d17d9
Blackdot <106665251+DarkShadowCoder@users.noreply.github.com>
2024-12-19 08:27:19 +0100

    Update FlightDao.kt

--- app/src/main/java/com/example/devmobile/FlightDao.kt
+++ app/src/main/java/com/example/devmobile/FlightDao.kt
@@ -1,15 +1,19 @@
 package com.example.devmobile
 
+import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import java.util.concurrent.Flow
 
-interface FlightDao {
-    @Insert(onConflict = onConflictStrategy.IGNORE)
-    suspend fun insert(favorite: Favorite)
+@Dao
+interface AirportDao {
+    @Query("SELECT * FROM airport WHERE name LIKE :query OR iata_code LIKE :query ORDER BY passengers DESC")
+    fun searchAirports(query: String): List<Airport>
 
-    @Query("SELECT * From airport WHERE iata_code = :code or name = :name")
-    fun getAirport(code: Int, name : String) : Flow<Airport>
+    @Query("SELECT * FROM favorite")
+    fun getFavorites(): List<Favorite>
 
-    @Query("SELECT * From airport")
-    fun getAllAirport() : Flow<Airport>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addFavorite(favorite: Favorite)
 }
